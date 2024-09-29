99fddbc0cc83eba6915d4da4b6b1128dad0e21e5
bldambtn <166319659+bldambtn@users.noreply.github.com>
2024-09-29 16:43:19 -0500

    updated with Onsen UI

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,28 @@
 // Import React to use JSX
 import React from "react";
+// Import Onsen UI components
+import { Page, Button } from 'react-onsenui'; 
 // Import the Project component for reuse
 import Project from "../components/Project"; 
 
 // Define the Portfolio component, which displays a list of projects
 function Portfolio() {
   return (
-    <div>
-      {/* Heading for the Portfolio section */}
-      <h1>Portfolio</h1>
+    <Page>
+      <h1>My Projects</h1>
 
-      {/* Example usage of the Project component to display individual projects */}
-      <Project title="Project 1" description="Description of project 1" />
-      <Project title="Project 2" description="Description of project 2" />
-      <Project title="Project 3" description="Description of project 3" />
-    </div>
+      {/* Add a button from Onsen UI */}
+      <Button onClick={() => alert("This is an Onsen UI Button!")}>
+        Click Me!
+      </Button>
+
+      {/* Display your projects using the Project component */}
+      <div className="projects-container">
+        <Project title="Project 1" description="Description of project 1" />
+        <Project title="Project 2" description="Description of project 2" />
+        <Project title="Project 3" description="Description of project 3" />
+      </div>
+    </Page>
   );
 }
 
